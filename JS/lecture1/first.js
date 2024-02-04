decfb0c3f903b3f8083af036d76e9752c151b927
Srijana Limbu <91357218+srijanalimbu55@users.noreply.github.com>
2024-02-04 11:22:22 +0545

    lecture 1 added

---
+++ JS/lecture1/first.js
@@ -0,0 +1,21 @@
+//1.create a const object called "product" to store information in the picture.
+const product = {
+    name: "Ball pen",
+    color: "black",
+    rating: 4,
+    offer: 5,
+    price: 270
+};
+console.log(product);
+//create a const object called "profile" to store information shown in the picture.
+const profile = {
+    username: "@l_srrrijana",
+    isFollow: false,
+    followers: 200,
+    following: 300
+};
+console.log(profile);
+console.log(typeof(profile));
+console.log("type of username:",typeof("username"));
+
+
