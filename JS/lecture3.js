fb3cae6dfb101d82cfe746942d913b3742e0f54b
Srijana Limbu <91357218+srijanalimbu55@users.noreply.github.com>
2024-02-05 06:56:35 +0545

    added lecture 3

---
+++ JS/lecture3.js
@@ -0,0 +1,87 @@
+//do while loop
+
+// let i = 1;
+// do {
+//     console.log("i =",i);
+//     i++;
+// } while (i <=5);
+
+//while loop
+// let i = 1;
+// while (i <=6 ) {
+//     console.log("i=", i);
+//     i++;
+// }
+
+// output:
+// "i=" 1
+// > "i=" 2
+// > "i=" 3
+// > "i=" 4
+// > "i=" 5
+// > "i=" 6
+
+//forloop
+// let om = "shivaya";
+// for(let i of om) {
+//     //iterator-> characters
+//     console.log("i =", i);
+// }
+
+// output:
+// > "i =" "s"
+// > "i =" "h"
+// > "i =" "i"
+// > "i =" "v"
+// > "i =" "a"
+// > "i =" "y"
+// > "i =" "a"
+
+
+// let str = "JavaScript";
+// let size = 0;
+// for (let i of str) {
+//     console.log("i = ",i)
+//     size++;
+// }
+// console.log("string size = ", size);
+//string size =10
+
+
+// let student = {
+//     name: "ram",
+//     isPass: true,
+//     age: 11
+//   }
+//   for (let i in student) {
+//     console.log(i);
+//   }
+
+//   output:
+// > "name"
+// > "isPass"
+// > "age"
+
+
+// let student = {
+//     name: "ram",
+//     isPass: true,
+//     age: 11
+//   }
+//   for (let key in student) {
+//     console.log("key =", key, "value =", student[key]);
+//   }
+
+//   output:
+//   > "key =" "name" "value =" "ram"
+// > "key =" "isPass" "value =" true
+// > "key =" "age" "value =" 11
+
+//1.print all even numbers from 0 to 100.
+
+// for(let i = 0; i<=100; i++) {
+//     if(i % 2 == 0) {
+//      console.log(i)
+//    }
+// }
+
